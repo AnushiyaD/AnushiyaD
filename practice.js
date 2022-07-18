776aa726f8882e33d1741c5bc5a6858fecb7c2e1
Anushiya <anushiyadhamu@gmail.com>
2022-07-18 19:52:43 +0530

    updated by anu second

--- practice.js
+++ practice.js
@@ -34,26 +34,3 @@ console.log(Name,Age,Address,Aadharno)
   res.end()
 })
 
-app.use(express.json());
-app.get('/demo',(req,res) =>{
- 
-   var Name = req.query.Name
-   var Age =  req.query.Age
-   var Address = req.query.Address
-   var Aadharno = req.query.Aadharno;
-console.log(Name,Age,Address,Aadharno)
-  
-  if(Aadharno.startsWith("1")){
-     var creditscore = {"score":200,"risklevel":"low","loanAmount":200000}
-    res.status(200).json(creditscore)
-  }
-  else if(Aadharno.startsWith("2")){
-    var creditscore = {"score":300,"risklevel":"high"}
-   res.status(200).json(creditscore)
-  }
-  else{
-    var creditscore = {"score":500,"risklevel":"zero"}
-    res.status(200).json(creditscore)
-  }
-  res.end()
-})
